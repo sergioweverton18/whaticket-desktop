a89b9d793ed9df37893c04b86b8a02529a12fd03
sergio <sergioweverton18@gmail.com>
2025-03-31 09:35:50 -0300

    initial

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL("https://seu-site.com"); // Ou use `mainWindow.loadFile("index.html");`
+});
